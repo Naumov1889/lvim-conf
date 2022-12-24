0b5df4c985d16f60f3a9caba3eff007afbb7bf18
maksim <maksim.naumov1889@gmail.com>
2022-12-24 11:07:30 +0500

    update leap plugin to disable auto-jumping

--- lua/user/leap.lua
+++ lua/user/leap.lua
@@ -1,1 +1,11 @@
-require('leap').opts.safe_labels.add_default_mappings()
+local status_ok, leap = pcall(require, "leap")
+if not status_ok then
+  return
+end
+
+leap.add_default_mappings({
+  opts = {
+    safe_labels = {}
+  }
+})
+leap.opts.safe_labels = {}
