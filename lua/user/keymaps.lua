218e9565f4c25f288a2312898030fe4d2809ec6e
maksim <maksim.naumov1889@gmail.com>
2022-12-26 18:06:14 +0500

    update keybinding to not yank in visual mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,15 +4,17 @@ lvim.leader = "space"
 -- jk or kj go to normal mode from insert mode
 lvim.keys.insert_mode["jk"] = '<Esc>'
 lvim.keys.insert_mode["kj"] = '<Esc>'
+
 -- L and H to navigate buffers
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- X closes a buffer
 lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
--- F Open Spectre (find and replace). Close with :q
+--
+-- R Open Spectre (find and replace). Close with :q
 lvim.keys.normal_mode["<S-r>"] = ":lua require('spectre').open()<CR>"
 lvim.keys.visual_mode["<S-r>"] = ":lua require('spectre').open_visual({select_word=true})<CR>"
-lvim.keys.normal_mode["<leader>R"] = ":lua require('spectre').open_file_search()<CR>"
+lvim.keys.normal_mode["<leader>r"] = ":lua require('spectre').open_file_search()<CR>"
 
 -- add empty line below and above without entering insert_mode
 lvim.keys.normal_mode["<CR>"] = "o<Esc>"
@@ -30,6 +32,10 @@ lvim.keys.visual_mode["<M-k>"] = ":m '<-2<CR>gv=gv"
 
 -- when paste don't yank selected text
 lvim.keys.visual_mode["p"] = '"_dP'
+-- when delete don't yank selected text
+lvim.keys.visual_mode["d"] = '"_d'
+-- when change don't yank selected text
+lvim.keys.visual_mode["c"] = '"_di'
 
 -- alt-s to leap to line
 lvim.keys.normal_mode["<M-s>"] = "<cmd>lua LEAP_TO_LINE()<CR>"
