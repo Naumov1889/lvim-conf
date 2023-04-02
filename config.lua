2cdeae268f025937cb0b8e4e0ca58dff76050441
Maksim Naumov <maksim.naumov1889@gmail.com>
2023-04-02 18:50:37 +0500

    nvimtree update_cwd=false

--- config.lua
+++ config.lua
@@ -15,6 +15,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.update_cwd = false
 
 lvim.builtin.treesitter.auto_install = true
 
