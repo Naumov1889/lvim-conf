50b39cc19be36a9d01571c8534b62a592955b1ff
maksim <maksim.naumov1889@gmail.com>
2023-04-08 10:54:35 +0500

    nvimtree.update_focused_file.update_root = false

--- config.lua
+++ config.lua
@@ -18,7 +18,7 @@ lvim.builtin.terminal.auto_scroll = false
 -- https://github.com/nvim-tree/nvim-tree.lua/blob/master/doc/nvim-tree-lua.txt
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.nvimtree.setup.update_cwd = false
+lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
 
 -- LunarVim igonores has this option set to hide node_modules, .git, etc.
 -- If you don't want that set it to empty obj. Or use toggle custom filter pressing U
