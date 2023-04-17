954041c93478704f40ea440f952ef1df2a7a2da7
Maksim Naumov <maksim.naumov1889@gmail.com>
2023-04-17 12:11:57 +0500

    codeium autocomplete-accept binding update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,9 +2,15 @@ lvim.plugins = {
   "ggandor/leap.nvim",
   "windwp/nvim-spectre",
   "mbbill/undotree",
-  "Exafunction/codeium.vim",
-
-
+  {
+    'Exafunction/codeium.vim',
+    config = function ()
+      vim.keymap.set('i', '<M-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+      -- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      -- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      -- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+  }
 
   --     {
   --       "folke/trouble.nvim",
